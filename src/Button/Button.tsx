9f2ca45a9243b6d681828168534143050eb86b00
Alexandros Karagiannis <pp.alexkara@gmail.com>
2023-05-22 18:06:07 -0600

    wip broke test

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -10,6 +10,6 @@ export interface ButtonProps  {
 
 export const Button :FC<ButtonProps>= ({text,color,cb,size}) => {
 
-    
-    return <button onClick={cb} style={{backgroundColor:color,height:size+'px',width:size*2+'px'}}>{text}</button>
+
+    return <button onClick={cb} style={{backgroundColor:'yellow',height:size+'px',width:size*2+'px'}}>{text}</button>
 }
